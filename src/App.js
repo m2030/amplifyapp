e9901d6348f31d61b90bcb7c8fa4b4d8f2e73c80
m2030 <30866381+m2030@users.noreply.github.com>
2020-06-17 16:32:55 +0200

    update src app
    change some text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>
+    first AWS amplify test</code>.
         </p>
         <a
           className="App-link"
